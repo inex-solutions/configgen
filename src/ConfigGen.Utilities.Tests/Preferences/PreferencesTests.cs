fb29682d481b4d9b093380cf697742264d53b9c4
Rob Levine <rob@roblevine.co.uk>
2016-08-01 18:50:14 +0100

    experiments with preferences

---
+++ src/ConfigGen.Utilities.Tests/Preferences/PreferencesTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConfigGen.Utilities.Preferences;
+using Machine.Specifications;
+
+namespace ConfigGen.Utilities.Tests.Preferences
+{
+    [Subject(typeof(PreferencesManager))]
+    public abstract class PreferencesTestBase
+    {
+        protected static PreferencesManager Subject;
+
+        Establish context = () =>
+        {
+            var personPreferences = new PreferenceGroup<PersonPreferences>(
+                name: "Person Preferences",
+                preferences: new []
+                {
+                    new Preference<PersonPreferences>(
+                        name: "PersonName",
+                        shortName: "Name",
+                        preferenceType: PreferenceType.SingleArgument, 
+                        setter: (preferences, value) => preferences.PersonName = value),
+                    new Preference<PersonPreferences>(
+                        name: "PersonAge",
+                        shortName: "Age",
+                        preferenceType: PreferenceType.SingleArgument,
+                        setter: (preferences, value) => preferences.PersonAge = int.Parse(value)),
+                });
+
+            var housePreferences = new PreferenceGroup<HousePreferences>(
+                name: "House Preferences",
+                preferences: new[]
+                {
+                    new Preference<HousePreferences>(
+                        name: "HouseAddress",
+                        shortName: "Address",
+                        preferenceType: PreferenceType.SingleArgument,
+                        setter: (preferences, value) => preferences.Address = value),
+                    new Preference<HousePreferences>(
+                        name: "IsFlat",
+                        shortName: "Flat",
+                        preferenceType: PreferenceType.Switch,
+                        setter: (preferences, value) => preferences.IsFlat = bool.Parse(value)),
+                });
+
+            Subject = new PreferencesManager(personPreferences, housePreferences);
+        };
+    }
+
+    public class PersonPreferences
+    {
+        public string PersonName { get; set; }
+
+        public int PersonAge { get; set; }
+    }
+
+    public class HousePreferences
+    {
+        public string Address { get; set; }
+
+        public bool IsFlat { get; set; }
+    }
+    // public class test1 : PreferencesTestBase
+}
---
+++ src/ConfigGen.Utilities/Preferences/PreferenceGroup.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConfigGen.Utilities.Preferences
+{
+    public interface IPreferenceGroup
+    {
+        string Name { get; }
+
+    }
+
+    public interface IPreferenceGroup<TPreferences> : IPreferenceGroup
+    {
+        
+    }
+
+    public class PreferenceGroup<TPreferences> : IPreferenceGroup<TPreferences>
+    {
+        public PreferenceGroup(string name, IEnumerable<IPreference<TPreferences>> preferences)
+        {
+            Name = name;
+            Preferences = preferences;
+        }
+
+        public string Name { get; }
+
+        public IEnumerable<IPreference<TPreferences>> Preferences { get; }
+    }
+
+
+
+
+    public interface IPreference
+    {
+        string Name { get; }
+
+        string ShortName { get; }
+    }
+
+    public interface IPreference<in TPreferences>
+    {
+        void Set(TPreferences target, string value);
+    }
+
+    public class Preference<TPreference> : IPreference<TPreference>
+    {
+        private readonly Action<TPreference, string> _setter;
+
+        public Preference(
+            string name,
+            string shortName,
+            PreferenceType preferenceType,
+            Action<TPreference, string> setter)
+        {
+            _setter = setter;
+            Name = name;
+            ShortName = shortName;
+            PreferenceType = preferenceType;
+        }
+
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        public PreferenceType PreferenceType { get; set; }
+
+        public void Set(TPreference target, string value)
+        {
+            _setter(target, value);
+        }
+    }
+
+    public enum PreferenceType
+    {
+        SingleArgument,
+        Switch
+    }
+
+    public class PreferencesManager
+    {
+        private readonly IEnumerable<IPreferenceGroup> _preferences;
+
+        public PreferencesManager(IEnumerable<IPreferenceGroup> preferences)
+        {
+            _preferences = preferences;
+        }
+    }
+}

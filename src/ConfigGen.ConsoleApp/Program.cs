5ad795c4910bb0cdc4bf0cda965eda2c87a2b6bc
Rob Levine <rob@roblevine.co.uk>
2018-01-14 18:54:07 +0000

    Add console app

---
+++ src/ConfigGen.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConfigGen.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
